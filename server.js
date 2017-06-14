b104a4fe1dc6ad1565d26e899280e8608c66e0d7
jak <jak171646167@gmmail.com>
2017-06-14 15:35:24 +0700

    initial

---
+++ server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var path = require('path');
+users = [];
+connections = [];
+
+server.listen(3000,function(){
+	console.log('Server Running...');
+});
+	
+app.set('view engine','ejs');
+app.set('views',path.resolve(__dirname,'views'));
+
+app.use(express.static(path.resolve(__dirname)));
+
+app.get('/',function(req,res){
+	res.render('index.ejs');
+});
+
+io.sockets.on('connection',function(socket){
+	connections.push(socket);
+	console.log('Connected: %s socket Connected', connections.length);
+	//Disconnect
+	socket.on('disconnect',function(data){
+	
+		users.splice(users.indexOf(socket.username),1);
+		updateUsernames();
+		connections.splice(connections.indexOf(socket),1);
+		console.log('DisConnected: %s socket Connected', connections.length);
+	});
+	//Send Message
+	socket.on('send message',function(data){
+		
+		io.sockets.emit('new message',{msg: data, user: socket.username});
+	});
+	//New User
+	socket.on('new user', function(data, callback){
+		callback(true);
+		socket.username = data;
+		users.push(socket.username);
+		updateUsernames();
+	});
+
+	function updateUsernames(){
+		io.sockets.emit('get users', users);
+	}
+});
+
